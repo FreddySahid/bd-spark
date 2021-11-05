ed7313a32cb8b36fe1ca8adb428255489a79a078
FreddySahid <freddysahid@outlook.com>
2021-11-05 14:06:46 -0600

    Ya puedo agregar un usuario a la base de datos

--- src/main/java/mx/uv/bd/Conexion.java
+++ src/main/java/mx/uv/bd/Conexion.java
@@ -9,7 +9,7 @@ public class Conexion {
     private String url = "jdbc:mysql://127.0.0.1:3306/80688_21";
     private String driverName = "com.mysql.jdbc.Driver";
     private String user = "root";
-    private String password = "";
+    private String password = "123456";
     private Connection con = null;
 
     public Connection getConnection(){
